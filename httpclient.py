10ad7884407a205ac7308b9b83436966cabd8898
Abram Hindle <abram.hindle@softwareprocess.es>
2016-01-28 18:18:18 -0700

    Update Copyright

--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Copyright 2013 Abram Hindle
+# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
